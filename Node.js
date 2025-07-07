9d44621c7ec9e0354d6f44d90ab5f10c90c26bc0
zhoufuguo <zhoufuguo@163.com>
2025-07-07 17:54:26 +0800

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(4221, () => {
+  console.log('Server running on port 4221');
+});
+
+// 保持主线程运行（Node.js 会自动保持服务器运行）
